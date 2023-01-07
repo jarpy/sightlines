6d0cb9ebbceb4ac91e586cf350ed13389fd1afef
Toby McLaughlin <toby@jarpy.net>
2023-01-07 12:48:39 +1030

    Use CellRunner in main app

--- sightlines/main.py
+++ sightlines/main.py
@@ -1,14 +1,24 @@
 from random import randint
+from time import sleep
 
 from launchpad_py import LaunchpadPro
+from sightlines.cell import Cell
 from sightlines.grid import Grid
+from sightlines.cell_runner import CellRunner
+
 
 def main():
     hardware = LaunchpadPro()
     hardware.Open()
     hardware.Reset()
     grid = Grid(hardware=hardware)
+
+    def updater(cells: list[Cell]):
+        for cell in cells:
+            cell.set_rgb(randint(0, 127), randint(0, 127), randint(0, 127))
+
+    runner = CellRunner(cells=grid[1], update_function=updater, interval=1.0)
+    runner.start()
+
     while True:
-        for row in grid.cells:
-            for cell in row:
-                cell.set_rgb(randint(0, 127), randint(0, 127), randint(0, 127))
+        sleep(1)
