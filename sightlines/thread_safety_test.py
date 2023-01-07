78e1ce4b1abd002c1c4fc1277e5155fc9c762b3e
Toby McLaughlin <toby@jarpy.net>
2023-01-07 12:24:45 +1030

    Add thread safety test script

---
+++ sightlines/thread_safety_test.py
@@ -0,0 +1,45 @@
+"""An experiment to see if we can safely use the Launchpad from multiple threads."""
+from threading import Thread
+from sightlines.grid import Grid
+from launchpad_py.launchpad import LaunchpadPro
+
+
+def red(grid: Grid):
+    while True:
+        for row in grid.cells:
+            for cell in row:
+                cell.set_rgb(127, 0, 0)
+
+def green(grid: Grid):
+    while True:
+        for row in grid.cells:
+            for cell in row:
+                cell.set_rgb(0, 127, 0)
+
+def blue(grid: Grid):
+    while True:
+        for row in grid.cells:
+            for cell in row:
+                cell.set_rgb(0, 0, 127)
+
+
+def smoketest():
+    hardware = LaunchpadPro()
+    hardware.Open()
+    hardware.Reset()
+    grid = Grid(hardware=hardware)
+
+    threads = [
+        Thread(target=red, args=[grid]),
+        Thread(target=green, args=[grid]),
+        Thread(target=blue, args=[grid]),
+    ]
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+if __name__ == "__main__":
+    smoketest()
